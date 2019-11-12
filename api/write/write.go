b1dd1aa9a7057db2bc969f489b0b0fdc549b1b5c
mohamedgamaleldin <mohammad.jamaleldin@gmail.com>
2019-11-12 15:54:38 +0400

    Testing mongo.

--- api/write/write.go
+++ api/write/write.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"log"
+	_ "net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+/*func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "Write service called.")
+}*/
+
+type Post struct {
+	text string
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	/*http.HandleFunc("/", handler)
 	fmt.Println("Server is running...")
-	http.ListenAndServe("0.0.0.0:3000", nil)
+	http.ListenAndServe("0.0.0.0:3000", nil)*/
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("mongo connection success.")
+
+	collection := client.Database("test").Collection("posts")
+
+	post := Post{"Hello, mongo!"}
+
+	insertResult, err := collection.InsertOne(context.TODO(), post)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted document: ", insertResult.InsertedID)
+
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("mongo connection closed.")
 }
